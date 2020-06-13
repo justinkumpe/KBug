9f67b1b5c8904ed5e76e2cd45141b05b338db779
Justin Kumpe <jakumpe@kumpes.com>
2020-06-13 15:09:55 -0500

    Initial Commit

---
+++ KBug/KBug.h
@@ -0,0 +1,19 @@
+//
+//  KBug.h
+//  KBug
+//
+//  Created by Justin Kumpe on 6/13/20.
+//  Copyright © 2020 Justin Kumpe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KBug.
+FOUNDATION_EXPORT double KBugVersionNumber;
+
+//! Project version string for KBug.
+FOUNDATION_EXPORT const unsigned char KBugVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KBug/PublicHeader.h>
+
+
